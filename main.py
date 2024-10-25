041958c5c14370383137469572c7b498d6b75ea9
Chukwuemika11 <izunduchukwuemika9@gmail.com>
2024-10-25 23:21:05 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,12 @@
+print("hello world!")
+
+number = 20
+age = 29
+
+
+if (number > age):
+    print("I am older than you")
+elif (number < age):
+     print("I am not older than you")   
+else:
+     print("We are age mates")      
